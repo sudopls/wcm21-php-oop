fcd4dde91a6bf3a5987422ba8e242170f77afa93
Johan Nordström <johan@digitalvillage.se>
2022-02-17 14:56:02 +0100

    Show authenticated user.

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+/* Protected Routes */
+Route::middleware('auth')->group(function() {
+	Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
